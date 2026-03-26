bcf58c6103e6b36bd1bcae0166c6f36e6f2cfce9
Tarborar <mathieuseurre84@gmail.com>
2026-03-26 16:21:41 +0100

    page faq

--- src/Pages/Faq.jsx
+++ src/Pages/Faq.jsx
@@ -1,9 +1,128 @@
+import { useState } from 'react';
+import '../Styles/pages/Faq.scss';
+
+//Diamètre du cercle extérieur
+const size = 1100;
+const center = size / 2;
+const outerRadius = 520;
+
+//Diamètre du second cercle intérieur
+const innerRadius = 390;
+
+//Nombre de segment
+const segments = 3;
+
+//Amplitude de l'arc de cercle
+const spacing = 0.35;
+const startAngle = -Math.PI * (0.5 + spacing);
+const endAngle = -Math.PI * (0.5 - spacing);
+
+//Espace entre les segments
+const gapPx = 50;
+const gapAngle = gapPx / outerRadius;
+
+//Convertit les coordonnées (rayon, angle) en coordonnées (x, y) pour placer <path />
+function polarToCartesian(cx, cy, r, angle){
+    return {
+        x: cx + r * Math.cos(angle),
+        y: cy + r * Math.sin(angle),
+    }
+}
+
+function createSlicePath(i){
+    const totalAngle = endAngle - startAngle; //amplitude totale
+
+    const totalGap = gapAngle * (segments - 1); //amplitude totale des espaces
+
+    const usableAngle = totalAngle - totalGap; //amplitude totale des segments (sans les espaces)
+
+    const sliceAngle = usableAngle / segments; //amplitude de chaque segment[i]
+
+    const angleStart = startAngle + i * (sliceAngle + gapAngle); //position de départ du segment[i] + adapte le décalage pour segment[i+1]
+
+    const angleEnd = angleStart + sliceAngle; //position de fin du segment[i]
+
+    //Récupère les coordonnées (x, y) de fin et de début de l'arc[i] extérieur
+    const outerStart = polarToCartesian(center, center, outerRadius, angleStart);
+    const outerEnd = polarToCartesian(center, center, outerRadius, angleEnd);
+
+    //Récupère les coordonnées (x, y) de fin et de début de l'arc[i] intérieur
+    const innerStart = polarToCartesian(center, center, innerRadius, angleStart);
+    const innerEnd = polarToCartesian(center, center, innerRadius, angleEnd);
+
+    /*
+    (M)Move to
+    (A)Arc
+
+    (L)Line to
+    (A)Arc
+
+    (Z)Close path
+    */
+    return `
+        M ${outerStart.x} ${outerStart.y}
+        A ${outerRadius} ${outerRadius} 0 0 1 ${outerEnd.x} ${outerEnd.y}
+        L ${innerEnd.x} ${innerEnd.y}
+        A ${innerRadius} ${innerRadius} 0 0 0 ${innerStart.x} ${innerStart.y}
+        Z
+    `;
+}
+
 function Faq() {
+    const [faqClicked, setFaqClicked] = useState(null);
+
+    let inputContent;
+    let titleContent;
+    let answerContent;
+
+    switch(faqClicked){
+        case 0:
+            inputContent = "Menu 1";
+            titleContent = "Comment utiliser Discord ?"
+            answerContent = " Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam consectetuer adipiscing elit, sed diam nonummy nibh"
+            break;
+        case 1:
+            inputContent = "Menu 2";
+            titleContent = "Comment changer de musique ?"
+            answerContent = " Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat"
+            break;
+        case 2:
+            inputContent = "Menu 3";
+            titleContent = "Est-ce que c'est gratuit ?"
+            answerContent = " Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam consectetuer adipiscing elit, sed diam nonummy nibh consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam consectetuer adipiscing elit, sed diam nonummy nibh"
+            break;
+        default:
+            inputContent = "Une question ?";
+            titleContent = "Appuyez sur un bouton"
+            answerContent = " Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam consectetuer adipiscing elit, sed diam nonummy nibh"
+            break;
+    }
+
     return (
-    <div className="home center">
-        <h1>FAQ</h1>
-    </div>
-  )
+        <div className="faq vertical center gap">
+            <h2>FAQ</h2>
+            <svg
+                width={size}
+                height={size}
+                viewBox={`0 0 ${size} ${size}`}
+                className="faq-svg"
+            >
+                {Array.from({ length: segments }).map((_, i) => (
+                    <a key={i} href={`#${i}`}>
+                        <path 
+                            className='path' 
+                            d={createSlicePath(i)} 
+                            onClick={() => setFaqClicked(i)}
+                        />
+                    </a>
+                ))}
+            </svg>
+            
+            <button className='faq__button glass buttonPadding buttonText'>{inputContent}</button>
+            <h3 className='faq__title smallTitle'>{titleContent}</h3>
+            <p className='faq__paragraph paragraph glass'>{answerContent}</p>
+        </div>
+    )
 }
 
 export default Faq
